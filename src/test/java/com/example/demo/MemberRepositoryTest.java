c6a58fc0b3320e1e6db3244ecabd0a8ce69dacdd
midas123 <midasyk123@naver.com>
2020-08-22 19:48:41 +0900

    initial

---
+++ src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+	
+	@Autowired MemberRepository memberRepository;
+	
+	@Test
+	@Transactional //테스트에서 트랜잭션은 테스트 종료 후 롤백
+	@Rollback(false)
+	public void testMember() throws Exception{
+		//given
+		Member member = new Member();
+		member.setName("aaa");
+		
+		//when
+		Long savedId = memberRepository.save(member);
+		Member findMember = memberRepository.find(savedId);
+		
+		//then
+		Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+		Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+		Assertions.assertThat(findMember).isEqualTo(member);
+	}
+}
