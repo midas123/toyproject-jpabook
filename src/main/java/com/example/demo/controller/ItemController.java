0c01e0789c42cdecd241734f1647892d9df45f0d
midas123 <midasyk123@naver.com>
2020-08-30 13:30:04 +0900

    리팩토링: 서비스 클래스에 Dto -> 엔티티 업데이트 메서드 추가

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,86 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.item.Book;
+import com.example.demo.domain.item.Item;
+import com.example.demo.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "itemForm";
+    }
+
+    @PostMapping(value = "/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "itemList";
+    }
+
+    @GetMapping(value = "/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model
+            model) {
+        Book item = (Book) itemService.findOne(itemId); //down casting, 런타임 에러 발생 가능성 있음
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+        model.addAttribute("form", form);
+        return "updateItemForm";
+    }
+
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+        /*
+        * id 값을 조작해서 넘기는 경우에 대비해서 수정 폼을 내려줄때 id 값에 해당하는 사용자와
+        * 수정 form 제출시 id 값에 해당하는 사용자가 동일한지 검증하는 로직을 구현해야함
+        *
+        * */
+
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
+        itemService.updateBook(form.getId(),
+                form.getName(),
+                form.getPrice(),
+                form.getStockQuantity(),
+                form.getAuthor(),
+                form.getIsbn());
+        return "redirect:/items";
+    }
+
+}
--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -1,14 +1,13 @@
 package com.example.demo.service;
 
-import java.util.List;
-
+import com.example.demo.domain.item.Book;
+import com.example.demo.domain.item.Item;
+import com.example.demo.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.domain.item.Item;
-import com.example.demo.repository.ItemRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,8 +23,37 @@ public class ItemService {
 	public List<Item> findItems(){
 		return itemRepository.findAll();
 	}
+
+	@Transactional
+	public Item updateItem(Long itemId, Book param){
+		Item item = itemRepository.findOne(itemId);
+		item.updateItemStockAndPrice(param);
+		//item.setPrice(param.getPrice());
+		//item.setStockQuantity(param.getStockQuantity());
+		return item;
+	}
+
+	@Transactional
+	public void updateBook(Long itemId,
+						   String name,
+						   int price,
+						   int stockQuantity,
+						   String author,
+						   String isbn
+	){
+		Book item = (Book) itemRepository.findOne(itemId);
+		item.setName(name);
+		item.setPrice(price);
+		item.setStockQuantity(stockQuantity);
+		item.setAuthor(author);
+		item.setIsbn(isbn);
+	}
 	
 	public Item findOne(Long itemId) {
 		return itemRepository.findOne(itemId);
 	}
+
+
+
+
 }
