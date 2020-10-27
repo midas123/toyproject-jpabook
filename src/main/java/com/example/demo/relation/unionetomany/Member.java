992647bb6f4de3c673be1b54bacb5ba06438fa39
midas123 <midasyk123@naver.com>
2020-10-27 22:22:50 +0900

    1:N 양방향..좋은 관계 설정이 아님

--- src/main/java/com/example/demo/relation/unionetomany/Member.java
+++ src/main/java/com/example/demo/relation/unionetomany/Member.java
@@ -3,10 +3,7 @@ package com.example.demo.relation.unionetomany;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,4 +16,12 @@ public class Member {
 
     private String name;
 
+    /*
+    *연관 관계 주인이지만 읽기만 가능하도록 설정
+    *JPA 스펙에 공식적으로 1:N 양방향은 없음
+    *이 방식 보다는 N:1 양방향을 사용하는게 좋음
+    * */
+    @ManyToOne
+    @JoinColumn(name="team_id", insertable = false, updatable = false)
+    private Team team;
 }
