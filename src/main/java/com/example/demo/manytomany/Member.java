7e9952d9f13956c0582fe726b633c2862b1cf947
midas123 <midasyk123@naver.com>
2020-10-28 21:43:50 +0900

    N:M 양방향

--- src/main/java/com/example/demo/manytomany/Member.java
+++ src/main/java/com/example/demo/manytomany/Member.java
@@ -20,6 +20,9 @@ public class Member {
 
     /*
     * N:M 단방향
+    * N:M은 @JoinTable에 의해 연결 테이블이 생성됨
+    * 이 테이블은 양쪽 테이블의 참조키를 칼럼으로 하며,
+    * 다른 칼럼을 추가할 수 없는 단점이 있음
     * */
     @ManyToMany
     @JoinTable(name="MEMBER_PRODUCT")
--- src/main/java/com/example/demo/manytomany/Product.java
+++ src/main/java/com/example/demo/manytomany/Product.java
@@ -3,10 +3,9 @@ package com.example.demo.manytomany;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,4 +17,10 @@ public class Product {
     private Long id;
 
     private String name;
+
+    /*
+    * N:M 양방향
+    * */
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 }
